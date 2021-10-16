6dbc3acfe730e8a5156ba59ff2281e6ad4e3df9c
Christopher Boyle <chrismjboyle@gmail.com>
2021-10-16 15:42:00 +0100

    Before doing anything, initialise renderer.

--- src/monch/main.cpp
+++ src/monch/main.cpp
@@ -1,7 +1,9 @@
 #include <monch/editor/app/EditorApp.h>
+#include <monch/rendering/renderer/Renderer.h>
 
 int main()
 {
+    Renderer::ref();
     EditorApp &win = EditorApp::ref();
     win.run();
 }
